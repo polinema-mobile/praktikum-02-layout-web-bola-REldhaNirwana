69d052db58fe08419ce96b1524fda8af270c7d9f
= <=>
2021-02-19 12:13:23 +0700

    Praktikum Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,18 +6,31 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+  int _count = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-          floatingActionButton:FloatingActionButton( 
-              onPressed: () {
- // Add your onPressed code here!
-              },
-          child: Icon(Icons.thumb_up),
-            backgroundColor: Colors.pink,
-          ),
-        ),
-    );
+        
+ home: Scaffold(
+ appBar: AppBar(
+ title: Text('Sample Code'),
+ ),
+ body: Center(
+ child: Text('You have pressed the button $_count times.'), 
+ ),
+ bottomNavigationBar: BottomAppBar(
+ child: Container(
+ height: 50.0,
+ ),
+ ),
+ floatingActionButton: FloatingActionButton(
+ onPressed: () => 0,
+ tooltip: 'Increment Counter',
+ child: Icon(Icons.add),
+ ),
+ floatingActionButtonLocation:
+FloatingActionButtonLocation.centerDocked,
+ ),
+ );
  }
 } 
