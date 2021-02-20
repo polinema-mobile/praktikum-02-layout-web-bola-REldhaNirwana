bffe3dfc6a119bdf5cd14b38fa08756a5b582324
= <=>
2021-02-20 20:00:56 +0700

    memperbaiki gambar

--- lib/main.dart
+++ lib/main.dart
@@ -49,33 +49,37 @@ class MyApp extends StatelessWidget {
             SizedBox(),
                 Container(
                   decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple[300])
+                      image: const DecorationImage(
+                        alignment: Alignment.topCenter,
+                        image: NetworkImage('https://www.spurs-web.com/static/uploads/2019/07/skysports-diego-costa-atletico-madrid_4644146-696x392.jpg'),
+                        fit: BoxFit.fitWidth,
                       ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Image.network('https://www.spurs-web.com/static/uploads/2019/07/skysports-diego-costa-atletico-madrid_4644146.jpg'),
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: Center(
-                          child: Text(
-                            "Costa Mendekat Ke Palmeiras",
-                            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
-                            ),
-                        ),
+                      border: Border.all(
+                        color: Colors.purple,
+                        width:1,
                       ),
+
+                    ),
+                    alignment: Alignment.bottomCenter,
+                    child: Text(
+                        'Costa Mendekat Ke Palmeiras',
+                        style: TextStyle(fontSize: 20),
+
+                      ),
+
+                    height: 300,
+                    width:500,
+                    margin: const EdgeInsets.only(left: 1,right: 1, top:20)
+
+                    ),   
                     ]         
                    ), 
                   ),
                 ],
               ),
-            ),
-            ]
+            ),   
           ),
-        ),
-      ),
-    ),
+        ), 
   );
 }
 }
